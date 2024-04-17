0574815048ead386c638b1a50bef6eb271f284ab
Nitsu47 <agustraversa2004@gmail.com>
2024-04-17 08:23:20 -0700

    typing fix, jsonify and port fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,5 +20,5 @@ app.register_blueprint(app_views, url_prefix="api/v1")
 
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = os.environ.get("HBNB_API_PORT", "5000")
+    port = int(os.environ.get("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Index module """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 
 
-@app_views.route("/status", app_views)
+@app_views.route("/status")
 def json_str():
-    """Returns a JSON str"""
-    return {"status": "OK"}
+    """Returns status in a JSON dic"""
+    return jsonify({"status": "OK"})
