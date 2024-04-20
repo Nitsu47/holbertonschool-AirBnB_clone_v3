abeb42228fa862ae8223c247b47984142126eea0
Nitsu47 <agustraversa2004@gmail.com>
2024-04-20 22:06:22 +0000

    function to handle the 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,12 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handles the 404 not found error"""
+    return jsnoify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", "5000"))
