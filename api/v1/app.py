02761a670fe100c4ac02389f9701a6781fbc70e5
German Silveira <adolfogsilveira@gmail.com>
2024-04-19 21:05:58 -0700

    Getenv an storage close change in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,12 +12,12 @@ app.register_blueprint(app_views, url_prefix="api/v1")
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
     """Teardown method to manage app context"""
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", "5000"))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
